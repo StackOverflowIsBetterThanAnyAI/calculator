bcc8470c1a903308f20d0d4f940a80605f75d60f
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-08-16 18:37:10 +0200

    responsive

--- src/components/CalculatorButton.tsx
+++ src/components/CalculatorButton.tsx
@@ -13,7 +13,7 @@ const CalculatorButton = ({
 }: CalculatorButtonProps) => {
     return (
         <button
-            className={`aspect-square w-full rounded-full text-3xl hover:opacity-85 active:opacity-60 focus-visible:outline-3 outline-zinc-50 ${bgColor} drop-shadow-gray-700/20 drop-shadow-xl`}
+            className={`aspect-square w-full rounded-full text-xl xs:text-3xl hover:opacity-85 active:opacity-60 focus-visible:outline-3 outline-zinc-50 ${bgColor} drop-shadow-gray-700/20 drop-shadow-xl`}
             onClick={handleClick}
             data-testid={`button-${buttonText}`}
         >
--- src/components/CalculatorDisplay.tsx
+++ src/components/CalculatorDisplay.tsx
@@ -22,7 +22,7 @@ const CalculatorDisplay = () => {
 
     return (
         <div
-            className="text-2xl flex flex-col justify-between bg-slate-700 p-3 h-36 outline-2 outline-zinc-500/50"
+            className="text-xl xs:text-3xl flex flex-col justify-between bg-slate-700 p-3 h-32 xs:h-36 outline-2 outline-zinc-500/50"
             data-testid="display"
         >
             <span>
