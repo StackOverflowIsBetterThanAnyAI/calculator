525ddebf3ca3f545fd927953a2b99194c7701e7f
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-08-16 13:23:36 +0200

    types

--- src/hooks/useFocusTrap.ts
+++ src/hooks/useFocusTrap.ts
@@ -1,7 +1,9 @@
 import { useEffect } from 'react'
 import { type TableCharacterProps } from '../constants/constants'
 
-export const useFocusTrap = (tableCharacters: TableCharacterProps[]) => {
+type useFocusTrapProps = { tableCharacters: TableCharacterProps[] }
+
+export const useFocusTrap = ({ tableCharacters }: useFocusTrapProps) => {
     useEffect(() => {
         const handleFocusTrap = (e: KeyboardEvent) => {
             if (
